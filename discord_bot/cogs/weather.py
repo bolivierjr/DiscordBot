93361efb21bb37c0c1a014554e047bb63d85a198
Bruce Olivier <bruce.olivier@bracketlabs.com>
2021-11-13 23:15:55 -0600

    Created a new createdb command in the weather cog and added an error handler

--- discord_bot/cogs/weather.py
+++ discord_bot/cogs/weather.py
@@ -1,6 +1,9 @@
 import logging
 
 from discord.ext import commands
+from peewee import DatabaseError
+
+from ..models.users import User
 
 log = logging.getLogger(__name__)
 
@@ -16,9 +19,23 @@ class Weather(commands.Cog):
     async def on_ready(self):
         log.info(f"{__class__.__name__} cog is loaded and ready")
 
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx: commands.Context, error: commands.CommandError):
+        message: str = ""
+        if isinstance(error, DatabaseError):
+            message = str(error)
+
+        if message:
+            ctx.send(message, delete_after=10)
+
     ##########
     # Commands
     ##########
+    @commands.command()
+    @commands.is_owner()
+    async def createdb(self, ctx: commands.Context):
+        result: str = User.create_tables()
+        await ctx.send(f"> {result}")
 
 
 def setup(client: commands.Bot):
